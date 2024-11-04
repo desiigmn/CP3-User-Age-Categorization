cd684144549ff118d5c202278b595a9f3a57387d
Desiree Ann Guzman <desireeann000@gmail.com>
2024-11-04 18:00:53 +0800

    Commit with userAge.js

---
+++ userAge.js
@@ -0,0 +1,15 @@
+let age = "20";
+
+if (age < 5) {
+    console.log("You're a Toddler.");
+} else if (age <= 12) {
+    console.log("You're a Child.");
+} else if (age <= 19) {
+    console.log("You're a Teenager.");
+} else if (age <= 35) {
+    console.log("You're a Young Adult.");
+} else if (age <= 60) {
+    console.log("You're Middle-Aged.");
+} else {
+    console.log("You're a Senior.");
+}
